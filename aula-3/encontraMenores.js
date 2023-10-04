5501119d17c0a919714da22c86f4868e67ce905a
lucasjdc <lucasjpdc@gmail.com>
2023-10-03 22:52:28 -0300

    Ordenação pelo pivô

--- aula-3/encontraMenores.js
+++ aula-3/encontraMenores.js
@@ -23,4 +23,21 @@ function trocaLugar(array, de, para) {
     array[de] = elemento2;
 }
 
-console.log(encontraMenores(listaLivros[2], listaLivros));
+function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    console.log(pivo);
+    encontraMenores(pivo, array);
+    let valoresMenores = 0;
+    for(let analisando = 0; analisando < array.length; analisando++) {
+        let atual = array[analisando];
+        if (atual.preco <= pivo.preco && atual !==  pivo) {
+            trocaLugar(array, analisando,  valoresMenores);
+            valoresMenores++;
+
+        }
+    }
+    return array;
+}
+
+console.log(divideNoPivo(listaLivros));
+//console.log(encontraMenores(listaLivros[2], listaLivros));
